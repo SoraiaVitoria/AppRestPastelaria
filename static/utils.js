1add005206d6eaa2429fba7d7ba04197906fde06
Soraia Vitoria <soraia.erik@gmail.com>
2022-11-29 23:10:23 -0300

    fixes

--- static/utils.js
+++ static/utils.js
@@ -1,3 +1,8 @@
+$("#telefone").mask("(00) 0000-0000");
+$("#cpf").mask("000.000.000-00");
+$("#compra-fiado").mask("0000");
+$("#dia-fiado").mask("0000");
+
 function readURL(input) {
     if (input.files && input.files[0]) {
       var reader = new FileReader();
@@ -8,4 +13,18 @@ function readURL(input) {
   
       reader.readAsDataURL(input.files[0]);
     }
+}
+
+switch($('#grupo option:selected').val()){
+  case '0':
+    $('#grupo option:selected').html('Atendente')
+  break;
+  case '1':
+    $('#grupo option:selected').html('Caixa')
+  break;
+  case '2':
+    $('#grupo option:selected').html('Adm')
+  break;
+  default:
+    $('#grupo option:selected').html('')
 }
